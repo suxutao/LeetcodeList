fb25808bdccee3ba9248a00ce1ab0085c608e6fa
suxutao <113399058+suxutao@users.noreply.github.com>
2025-01-26 11:08:37 +0800

    1.26

--- cn/40-combination-sum-ii .cpp
+++ cn/40-combination-sum-ii .cpp
@@ -4,42 +4,34 @@ using namespace std;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
 public:
-    vector<vector<int>>vv;
-    vector<int>v;
-    vector<bool>vb;
-    void hui(vector<int>&can,int target,int pos){
-        int sum=0;
-        for (int i = 0; i < v.size(); ++i) {
-            sum+=v[i];
-        }
-        if (sum==target){
-            vv.push_back(v);
-            return;
-        }else if (sum>target){
-            return;
-        }else{
-            for (int i = pos; i < can.size(); ++i) {
-                if (i>0&&can[i]==can[i-1]&&!vb[i-1])
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        int n=candidates.size();
+        vector<int>v;
+        vector<vector<int>>ans;
+        ranges::sort(candidates);
+        auto dfs=[&](auto&&dfs,int pos,int sum)->void {
+            if (sum==target){
+                ans.push_back(v);
+                return;
+            }
+            for (int i = pos; i < n; ++i) {
+                if (candidates[i]+sum>target)
+                    break;
+                if (i>pos&&candidates[i]==candidates[i-1])
                     continue;
-                v.push_back(can[i]);
-                vb[i]=true;
-                hui(can, target, i + 1);
-                vb[i]=false;
+                v.push_back(candidates[i]);
+                dfs(dfs,i+1,sum+candidates[i]);
                 v.pop_back();
             }
-        }
-    }
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
-        vb.resize(candidates.size(), false);
-        hui(candidates,target,0);
-        return vv;
+        };
+        dfs(dfs,0,0);
+        return ans;
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
 
 
 int main(){
-    
+
     return 0;
 }
