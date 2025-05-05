2a04ed5b56a1f0b8f085fb81a0b90fdc214dd8fc
suxutao <113399058+suxutao@users.noreply.github.com>
2025-05-05 10:30:30 +0800

    5.5

--- cn/790-domino-and-tromino-tiling .cpp
+++ cn/790-domino-and-tromino-tiling .cpp
@@ -6,6 +6,8 @@ class Solution {
 public:
     const int N=1e9+7;
     int numTilings(int n) {
+        if (n==1)
+            return 1;
         vector<long long>dp(n+1);
         dp[1]=1;dp[2]=2;dp[0]=1;
         for (int i = 3; i <= n; ++i) {
