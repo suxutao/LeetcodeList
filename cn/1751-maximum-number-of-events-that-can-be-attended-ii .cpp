abee373384ea4d4097cd581fc509626af46f64dc
suxutao <113399058+suxutao@users.noreply.github.com>
2025-07-08 14:45:00 +0800

    7.8

--- cn/1751-maximum-number-of-events-that-can-be-attended-ii .cpp
+++ cn/1751-maximum-number-of-events-that-can-be-attended-ii .cpp
@@ -5,19 +5,20 @@ using namespace std;
 class Solution {
 public:
     int maxValue(vector<vector<int>> &events, int k) {
-        sort(events.begin(), events.end(), [](vector<int>& a,vector<int>&b) {
-            return a[1] < b[1];
+        int n=events.size();
+        vector<vector<int>>dp(n+1,vector<int>(k+1));
+        ranges::sort(events,[](auto&a,auto&b){
+            return a[1]<b[1];
         });
-        int n = events.size(), f[n+1][k+1];
-        memset(f, 0, sizeof(f));
         for (int i = 0; i < n; ++i) {
-            int p = lower_bound(events.begin(), events.begin() + i, events[i][0],[](auto &e, int lower) {
-                return e[1] < lower;
+            int p= lower_bound(events.begin(),events.end(),events[i][0],[](auto&a,int low){
+                return a[1]<low;
             })-events.begin();
-            for (int j = 1; j <= k; ++j)
-                f[i + 1][j] = max(f[i][j], f[p][j - 1] + events[i][2]);
+            for (int j = 1; j <= k; ++j) {
+                dp[i+1][j]= max(dp[i][j],dp[p][j-1]+events[i][2]);
+            }
         }
-        return f[n][k];
+        return dp[n][k];
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
