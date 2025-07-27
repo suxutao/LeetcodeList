b27fba6449fa61fc8353d20b54fb1635b0dd1a6d
suxutao <113399058+suxutao@users.noreply.github.com>
2025-07-27 10:29:35 +0800

    7.27

--- cn/2735-collecting-chocolates .cpp
+++ cn/2735-collecting-chocolates .cpp
@@ -4,8 +4,24 @@ using namespace std;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
 public:
-    long long minCost(vector<int>& nums, int x) {
-        
+    using ll = long long;
+    long long minCost(vector<int>& nums, ll x) {
+        int n=nums.size();
+        ll ans=LLONG_MAX,sum;
+        ranges::reverse(nums);
+        vector<int>mn(nums);
+        for (int i = 0; i < n; ++i) {
+            nums.push_back(nums[i]);
+        }
+        for (int i = 0; i < n; ++i) {
+            sum=x*i;
+            for (int j = 0; j < n; ++j) {
+                mn[j]=min(mn[j],nums[i+j]);
+                sum+=mn[j];
+            }
+            ans= min(ans,sum);
+        }
+        return ans;
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
