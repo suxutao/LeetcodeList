53f7ae4f101d8ea4610cf6181cf57939fd3386cc
suxutao <113399058+suxutao@users.noreply.github.com>
2026-02-09 18:52:09 +0800

    2.9

--- cn/1382-balance-a-binary-search-tree .cpp
+++ cn/1382-balance-a-binary-search-tree .cpp
@@ -15,25 +15,25 @@ using namespace std;
  */
 class Solution {
 public:
-    basic_string<int>s;
+    vector<int>s;
     void dfs(TreeNode*p){
         if (!p)return;
         dfs(p->left);
         s.push_back(p->val);
         dfs(p->right);
     }
-    TreeNode* build(basic_string<int>b){
-        int si=b.size();
-        if (si==0) return nullptr;
-        if (si==1) return new TreeNode(b[0]);
-        TreeNode*p=new TreeNode(b[si/2]);
-        p->left= build(b.substr(0,si/2));
-        p->right= build(b.substr(si/2+1,si-si/2-1));
+    TreeNode* build(int l,int r){
+        if (l>r) return nullptr;
+        if (l==r) return new TreeNode(s[l]);
+        int mid=(l+r)>>1;
+        TreeNode*p=new TreeNode(s[mid]);
+        p->left= build(l,mid-1);
+        p->right= build(mid+1,r);
         return p;
     }
     TreeNode* balanceBST(TreeNode* root) {
         dfs(root);
-        return build(s);
+        return build(0,s.size()-1);
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
